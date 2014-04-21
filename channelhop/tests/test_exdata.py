b7042d61f09b778da6da37b580de1af315c2f38c
Alex Corrie <ajccode@gmail.com>
2014-04-21 21:45:46 +0100

    Tests for the channelhop.exdata module

--- channelhop/tests/test_exdata.py
+++ channelhop/tests/test_exdata.py
@@ -1,18 +1,34 @@
 import unittest
-import exdata
-from places import LocationMap
+from datetime import datetime, timedelta
+import channelhop.exdata as exdata
+from channelhop.places import LocationMap
 
-class TestLoader(unittest.TestCase):
+FERRY_DATA = """
+Portsmouth,Cherbourg,Operator A,2000-01-02,09:30,2000-01-02,13:00,170,0,
+Portsmouth,Le Havre,Operator B,2000-01-01,23:00,2000-01-02,08:00,75,85.5,4-berth Cabin
+Portsmouth,Le Havre,Operator B,2000-01-02,23:00,2000-01-03,08:00,106.5,110,4-berth Cabin
+Poole,Cherbourg,Operator C,2000-01-02,08:30,2000-01-02,13:00,160,0,
+Cherbourg,Portsmouth,Operator A,2000-01-04,17:00,2000-01-04,19:00,170,0,
+Cherbourg,Poole,Operator C,2000-01-04,18:30,2000-01-04,21:00,185,0,
+Le Havre,Portsmouth,Operator B,2000-01-04,17:00,2000-01-04,21:00,85.5,0,
+""".strip().split('\n')
+
+CAR_DATA = """
+A,Portsmouth,40,00:45,8.5,
+A,Poole,60,01:30,10,
+Le Havre,B,250,04:30,90,tolls
+Le Havre,B,300,05:00,70.5,
+Cherbourg,B,200,03:45,50,
+""".strip().split('\n')
+
+
+class TestParser(unittest.TestCase):
 	def setUp(self):
-		self.loader = exdata.Loader()
-		dirname = os.path.dirname(__file__)
-		datad = os.path.join(dirname, 'data')
-		load = self.loader.load
-		self.cardata = load(os.path.join(datad, 'car_routes.csv'))
-		self.ferrydata = load(
-				os.path.join(datad,	'ferry_crossings.csv'))
 		self.lmap = LocationMap('A', 'B')
-		self.locations = self.lmap.locations
+		self.parser = exdata.Parser(self.lmap)
+		raw_data = {'ferry' : FERRY_DATA, 'car' : CAR_DATA}
+		self.cardata, self.ferrydata = self.parser.parse(raw_data)
+		self.locations = {loc.town:loc for loc in self.lmap.locations}
 
 	def test_car_data_records(self):
 		"""Check the right number of car route records are retrieved.
@@ -26,9 +42,9 @@ class TestLoader(unittest.TestCase):
 		"""Test complete initial record is retrieved."""
 		route = self.cardata[0]
 		self.assertIsInstance(route, exdata.CarData)
-		self.assertEqual(route.origin, self.locations['A'])
+		self.assertEqual(route.source, self.locations['A'])
 		self.assertEqual(route.distance, 40)
-		self.assertEqual(route.duration, timedelta(0, 45, 0))
+		self.assertEqual(route.duration, timedelta(45*60))
 		self.assertEqual(route.cost, 8.5)
 	
 	def test_final_car_data(self):
@@ -43,7 +59,7 @@ class TestLoader(unittest.TestCase):
 		self.assertIsInstance(route, exdata.CarData)
 		self.assertEqual(route.destination,
 						 self.locations['Cherbourg'])
-		self.assertEqual(route.duration, timedelta(3, 45, 0))
+		self.assertEqual(route.duration, timedelta(3*3600 + 45*60))
 		self.assertEqual(route.cost, 50)
 
 	def test_toll_variant(self):
@@ -51,7 +67,7 @@ class TestLoader(unittest.TestCase):
 		route = self.cardata[4]
 		self.assertIsInstance(route, exdata.CarData)
 		self.assertEqual(route.destination, self.locations['B'])
-		self.assertEqual(route.duration, timedelta(4, 30, 0))
+		self.assertEqual(route.duration, timedelta(4 * 3600 + 30 * 60))
 		self.assertEqual(route.cost, 90)
 		self.assertEqual(route.note, 'tolls')
 
@@ -68,7 +84,7 @@ class TestLoader(unittest.TestCase):
 	def test_initial_ferry_record(self):
 		"""Test first ferry crossing record is retrieved."""
 		route = self.ferrydata[0]
-		self.assertEqual(route.origin, self.locations['Portsmouth'])
+		self.assertEqual(route.source, self.locations['Portsmouth'])
 		self.assertEqual(route.operator, 'Operator A')
 		self.assertEqual(route.dep, datetime(2000, 1, 2, 9, 30, 0))
 		self.assertEqual(route.cost, 170)
@@ -102,3 +118,6 @@ class TestLoader(unittest.TestCase):
 		route = self.ferrydata[1]
 		self.assertEqual(route.cost, 75)
 		self.assertEqual(route.note, '')
+
+if __name__ == '__main__':
+	unittest.main()

d3d6c6ab54740b3b2cb8c9f8ced5b92710f27aa9
Alex Corrie <ajccode@gmail.com>
2014-04-24 00:11:12 +0100

    Added base entities, waypoints and links

---
+++ channelhop/travel.py
@@ -0,0 +1,33 @@
+# coding: utf-8 
+
+class Waypoint(object):
+	"""A waypoint is a node in an itinerary.
+	
+	Waypoints have location and an optional date/time. Where the
+	latter is not specified, it makes the waypoint suitable for
+	merging with another at the same location and specified date/time.
+
+		location : places.Location instance
+		datetime : datetime.datetime instance (optional)
+	
+	"""
+	def __init__(self, location, datetime=None):
+		self.location = location
+		self.datetime = datetime
+
+class Link(object):
+	"""A link is a transition between waypoints.
+
+	Links have a duration, a cost and a note. Both duration and cost
+	are required; the duration may be used to calculate undefined
+	waypoint datetime attributes.
+
+		duration : datetime.timedelta instance
+		cost : financial cost of journey (fuel, fares, tolls, etc.)
+		note : journey description for disambig./clarity
+
+	"""
+	def __init__(self, duration, cost, note=''):
+		self.duration = duration
+		self.cost = cost
+		self.note = note
